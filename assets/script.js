7dfdf205825747f684f7e97bba43f738866370a6
M. K. <maxxki.agentur@gmail.com>
2025-04-06 20:23:44 +0200

    Delete assets/script.js

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-// Quantum Dots Animation (für alle Seiten)
-document.addEventListener('DOMContentLoaded', function() {
-    const quantumDots = document.getElementById('quantumDots');
-    if (quantumDots) {
-        // (Hier dein bestehender Code für die Quantenpunkte)
-    }
-});
